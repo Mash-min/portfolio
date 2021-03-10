ccfe22405a80af99ad7347b7d46fed528fc69a79
Mashiyyat <delossantos.mashiyyat@gmail.com>
2021-03-10 10:49:08 +0800

    Major Update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-	$('.carousel').carousel({
-		indicators:true,
-		fullWidth:true,
-		noWrap:true
-	});
+  $('.scrollspy').scrollSpy({
+	  scrollOffset: 500
+  });
   $(".button-collapse").sideNav();
 });
+
+let skills = document.getElementById('skills');
+let sample_projects = document.getElementById('sample_projects');
+var options = [
+	{selector: '#sample_projects', offset: 500, callback: function(el) {
+		Materialize.fadeInImage($(el));
+		sample_projects.style.display = '';
+	} },
+	{selector: '#skills', offset: 500, callback: function(el) {
+		Materialize.fadeInImage($(el));
+		skills.style.display = '';
+	} }
+  ];
+  Materialize.scrollFire(options);
